b42152a68bd5421b3d79d361434c4aabcefdf8db
Crystal Zhang <tuf69164@temple.edu>
2020-07-23 15:24:33 -0400

    Add project files.

---
+++ MerchantRegistration/MerchantRegistration.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MerchantRegistration.aspx.cs" Inherits="MerchantRegistration.MerchantRegistration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Merchant Registration</h1>
+        <div>
+
+            <br />
+            <asp:Label ID="lblName" runat="server" Text="Name:"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="lblEmail" runat="server" Text="Email:"></asp:Label>
+            <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Label ID="lblPassword" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="btnCreate" runat="server" OnClick="btnCreate_Click" Text="Create Account" />
+            <br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ MerchantRegistration/MerchantRegistration.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using Utilities;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MerchantRegistration
+{
+    public partial class MerchantRegistration : System.Web.UI.Page
+    {
+        DBConnect dbConnect = new DBConnect();
+        SqlCommand objCommand = new SqlCommand();
+        string apiKey;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            //create a random number generator to generate
+            //a apiKey
+            var key = new byte[32];
+            using (var generator = RandomNumberGenerator.Create())
+                generator.GetBytes(key);
+            string apiKey = Convert.ToBase64String(key);
+            RemoveSpecialChars(apiKey);
+
+            //make a str that contains all chars they u think should be
+            //in a api key
+            //iterate through the key and see if the key[i]
+            //is a char that is in the new str
+            //if it is then add that char into a new string
+
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_CreateMerchant";
+
+            objCommand.Parameters.AddWithValue("@email", email);
+            objCommand.Parameters.AddWithValue("@name", name);
+            objCommand.Parameters.AddWithValue("@password", password);
+            objCommand.Parameters.AddWithValue("@apiKey", apiKey);
+
+
+            dbConnect.DoUpdateUsingCmdObj(objCommand);
+
+            Response.Write("Acc created");
+
+        }
+
+        //method that removes special chars from a string
+        public static string RemoveSpecialChars(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z' 
+                    && str[i] >= 'a' && str[i] <= 'z'))
+                {
+                    sb.Append(str[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
